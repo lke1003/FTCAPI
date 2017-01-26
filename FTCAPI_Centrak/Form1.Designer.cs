f436e5d32128f3ee34ad9ca2475bc8e7ea1ab1f7
FILIPE <filipelao1003@gmail.com>
2017-01-26 16:23:51 +0800

    Initial Project

---
+++ FTCAPI_Centrak/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace FTCAPI_Centrak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOn = new System.Windows.Forms.Button();
+            this.btnOff = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOn
+            // 
+            this.btnOn.Location = new System.Drawing.Point(83, 236);
+            this.btnOn.Name = "btnOn";
+            this.btnOn.Size = new System.Drawing.Size(75, 23);
+            this.btnOn.TabIndex = 0;
+            this.btnOn.Text = "On";
+            this.btnOn.UseVisualStyleBackColor = true;
+            this.btnOn.Click += new System.EventHandler(this.btnOn_Click);
+            // 
+            // btnOff
+            // 
+            this.btnOff.Location = new System.Drawing.Point(233, 236);
+            this.btnOff.Name = "btnOff";
+            this.btnOff.Size = new System.Drawing.Size(75, 23);
+            this.btnOff.TabIndex = 1;
+            this.btnOff.Text = "Off";
+            this.btnOff.UseVisualStyleBackColor = true;
+            this.btnOff.Click += new System.EventHandler(this.btnOff_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(143, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "lblstatus";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 422);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOff);
+            this.Controls.Add(this.btnOn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOn;
+        private System.Windows.Forms.Button btnOff;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FTCAPI_Centrak/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices.ComTypes;
+using CardaxFTEIAPILib;
+using Scripting;
+
+namespace FTCAPI_Centrak
+{
+    public partial class Form1 : Form, IFTMiddleware2, IFTTrigger2
+    {
+        //Create the event input API. 
+        private IFTExternalEventInput2 iAPI;
+        //Used for connecting IFTMiddleware2 and IFTTrigger2 manually.
+        private int cookieMiddle;
+        private int cookieTrigger;
+        private IConnectionPoint icpMiddle;
+        private IConnectionPoint icpTrigger;
+        //Script file.
+        private FileSystemObject fsoScriptFile;
+        private int callCounter;
+        private DateTime callStart;
+        private TextStream tsScriptFile;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        [MTAThread]
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitialConnection();
+            label1.Text = "Disconnect";
+            label1.ForeColor = System.Drawing.Color.Red;
+        }
+
+        private void InitialConnection()
+        {
+            try
+            {
+                //Create the API component as a CardaxFTEIAPIClass object.
+                iAPI = new CardaxFTEIAPIClass();
+
+                //cboRegionCode.SelectedIndex = 0;
+                //optLogEvent_CheckedChanged(optLogEvent, new EventArgs());
+                //fsoScriptFile = new FileSystemObjectClass();
+
+                //Connect to the IFTMiddleware2 interface (i.e. a non-default outbound interface on the API).
+                IConnectionPointContainer icpc = (IConnectionPointContainer)iAPI;
+                Guid guidMiddle = new Guid("ED5CDD41-4393-4b88-BF30-F250905D57C8");
+                icpc.FindConnectionPoint(ref guidMiddle, out icpMiddle);
+                icpMiddle.Advise(this, out cookieMiddle);
+
+                //Connect to the IFTTrigger2 interface (i.e. a non-default outbound interface on the API).
+                Guid guidTrigger = new Guid("C6BDE0BA-B4B1-495e-BE28-0D68A5C9AFFA");
+                icpc.FindConnectionPoint(ref guidTrigger, out icpTrigger);
+                icpTrigger.Advise(this, out cookieTrigger);
+            }
+            catch (Exception exception)
+            {
+                String sText = "Error while loading: " + exception.Message;
+
+                MessageBox.Show(sText, "API error", MessageBoxButtons.OK);
+            }
+        }
+
+        public void notifyItemDeregistered(string sSystemID, string sItemID)
+        {
+           // txtLog.Text = txtLog.Text + "notifyItemDeregistered. sSystemID: " + sSystemID + ", sItemID: " + sItemID + "\r\n";
+        }
+        public void notifyItemRegistered(string sSystemID, string sItemID, string sConfig)
+        {
+            //txtLog.Text = txtLog.Text + "notifyItemRegistered. sSystemID: " + sSystemID + ", sItemID: " + sItemID + ", sConfig: " + sConfig + "\r\n";
+            if (label1.InvokeRequired)
+            {
+                label1.Invoke(new MethodInvoker(delegate { label1.Text = "Connect "+ sSystemID + " " + sItemID; label1.ForeColor = System.Drawing.Color.Green; }));
+            }
+            //if (name == "MyName")
+            //{
+            //    // do whatever
+            //}
+            //label1.Text = "Connect";
+            //label1.ForeColor = System.Drawing.Color.Green;
+        }
+        public void notifySystemDeregistered(string sSystemID)
+        {
+           // txtLog.Text = txtLog.Text + "notifySystemDeregistered. sSystemID: " + sSystemID + "\r\n";
+        }
+        public void notifySystemRegistered(string sSystemID, string sTypeID, string sConfig)
+        {
+           // txtLog.Text = txtLog.Text + "notifySystemRegistered. sSystemID: " + sSystemID + ", sTypeID: " + sTypeID + ", sConfig: " + sConfig + "\r\n";
+        }
+        public void notifyAlarmAcknowledged(string sSystemID, int lEventID)
+        {
+           // txtLog.Text = txtLog.Text + "notifyAlarmAcknowledged. sSystemID: " + sSystemID + ", EventID: " + Convert.ToString(lEventID) + "\r\n";
+        }
+
+        /*
+         * The following methods represent the implementaion of IFTTrigger2 interface.
+         */
+        public void triggerEvent(string sSystemID, string sItem, string sTrigger)
+        {
+            //txtTrigger.Text = txtTrigger.Text + "triggerEvent. sSystemID: " + sSystemID + ", sItem: " + sItem + ", sTrigger: " + sTrigger + "\r\n";
+        }
+        public void triggerOutput(string sSystemID, string sItem, int lState, string sTrigger)
+        {
+            //txtTrigger.Text = txtTrigger.Text + "triggerOutput. sSystemID: " + sSystemID + ", sItem: " + sItem + ", lState: " + Convert.ToString(lState) + ", sTrigger: " + sTrigger + "\r\n";
+        }
+
+        private void btnOn_Click(object sender, EventArgs e)
+        {
+            DateTime dNow = DateTime.Now;
+            DateTime dNowUTC = dNow.ToUniversalTime();
+            iAPI.notifyStatus("ES01","ESI01", 1, 0, 0, "Alarm Trigger");
+            iAPI.logEvent(1, 1, dNowUTC, 1, "ES01", "ESI01", "Alarm On", "Alarm Trigger");
+        }
+
+        private void btnOff_Click(object sender, EventArgs e)
+        {
+            iAPI.notifyStatus("ES01", "ESI01", 0, 0, 0, "Resume Normal");
+            iAPI.notifyRestore(1, "ES01", "ESI01");
+        }
+
+
+    }
+}
